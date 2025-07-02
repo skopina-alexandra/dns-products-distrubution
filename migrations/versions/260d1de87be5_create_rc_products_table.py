14bbaa0da602948571df6e788811f698495be6ab
Alexandra Skopina <44308291+skopina-alexandra@users.noreply.github.com>
2025-07-02 01:45:13 +0000

    migration - create table dc_products

---
+++ migrations/versions/260d1de87be5_create_rc_products_table.py
@@ -0,0 +1,41 @@
+"""create dc_products table
+
+Revision ID: 260d1de87be5
+Revises: 4c8b06f3f3b2
+Create Date: 2025-07-02 01:32:08.981817
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+from sqlalchemy import text
+
+
+# revision identifiers, used by Alembic.
+revision: str = '260d1de87be5'
+down_revision: Union[str, Sequence[str], None] = '4c8b06f3f3b2'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    sql = text("""
+    create table dc_products(
+        dc_id uuid not null references distribution_centers(id) on delete cascade,
+        product_id uuid not null references products(product_id) on delete cascade,
+        stock_quantity integer not null check (stock_quantity >= 0),
+        reserve_quantity integer not null check (reserve_quantity >= 0),
+        transit_quantity integer not null check (transit_quantity >= 0)
+    )
+    """)
+    connection = op.get_bind()
+    connection.execute(sql)
+
+
+def downgrade() -> None:
+    sql = text("""
+    drop table if exists dc_products
+    """)
+    connection = op.get_bind()
+    connection.execute(sql)
+
