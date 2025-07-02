7c31ad1bc04cd47880d2bdb15b0186b580f5eaa2
Alexandra Skopina <skopina-aa@yandex-team.ru>
2025-07-02 17:52:15 +1000

    remove quantity positive check

--- migrations/versions/260d1de87be5_create_rc_products_table.py
+++ migrations/versions/260d1de87be5_create_rc_products_table.py
@@ -5,6 +5,7 @@ Revises: 4c8b06f3f3b2
 Create Date: 2025-07-02 01:32:08.981817
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,30 +13,33 @@ from sqlalchemy import text
 
 
 # revision identifiers, used by Alembic.
-revision: str = '260d1de87be5'
-down_revision: Union[str, Sequence[str], None] = '4c8b06f3f3b2'
+revision: str = "260d1de87be5"
+down_revision: Union[str, Sequence[str], None] = "4c8b06f3f3b2"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    sql = text("""
+    sql = text(
+        """
     create table dc_products(
         dc_id uuid not null references distribution_centers(id) on delete restrict,
         product_id uuid not null references products(product_id) on delete restrict,
-        stock_quantity integer not null check (stock_quantity >= 0),
-        reserve_quantity integer not null check (reserve_quantity >= 0),
-        transit_quantity integer not null check (transit_quantity >= 0)
+        stock_quantity integer not null,
+        reserve_quantity integer not null,
+        transit_quantity integer not null
     )
-    """)
+    """
+    )
     connection = op.get_bind()
     connection.execute(sql)
 
 
 def downgrade() -> None:
-    sql = text("""
+    sql = text(
+        """
     drop table if exists dc_products
-    """)
+    """
+    )
     connection = op.get_bind()
     connection.execute(sql)
-
--- migrations/versions/b276eae9043d_create_branch_products_table.py
+++ migrations/versions/b276eae9043d_create_branch_products_table.py
@@ -5,36 +5,40 @@ Revises: 260d1de87be5
 Create Date: 2025-07-02 01:57:10.094667
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
 from sqlalchemy import text
 
 # revision identifiers, used by Alembic.
-revision: str = 'b276eae9043d'
-down_revision: Union[str, Sequence[str], None] = '260d1de87be5'
+revision: str = "b276eae9043d"
+down_revision: Union[str, Sequence[str], None] = "260d1de87be5"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
-
 def upgrade() -> None:
-    sql = text("""
+    sql = text(
+        """
     create table branch_products(
         branch_id uuid not null references stores(branch_id) on delete restrict,
         product_id uuid not null references products(product_id) on delete restrict,
-        stock_quantity integer not null check (stock_quantity >= 0),
-        reserve_quantity integer not null check (reserve_quantity >= 0),
-        transit_quantity integer not null check (transit_quantity >= 0)
+        stock_quantity integer not null,
+        reserve_quantity integer not null,
+        transit_quantity integer not null
     )
-    """)
+    """
+    )
     connection = op.get_bind()
     connection.execute(sql)
 
 
 def downgrade() -> None:
-    sql = text("""
+    sql = text(
+        """
     drop table if exists branch_products
-    """)
+    """
+    )
     connection = op.get_bind()
     connection.execute(sql)
