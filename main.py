8f2c73a22dacbd2d355d523210d0933e8554e193
Alexandra Skopina <skopina-aa@yandex-team.ru>
2025-07-03 11:19:40 +1000

    load env and set logging level

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 # from src.distribute import distribute
 from src.distribute import distribute
+from dotenv import load_dotenv
+import logging
 
 if __name__ == "__main__":
+
+    load_dotenv()
+    logging.basicConfig(level=logging.INFO)
 
     distribute()
